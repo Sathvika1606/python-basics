1aa82b2cdbc35db7c4e41b00a414f4fec1edb8e8
Sathvika1606 <Sathvikalatha1458@gmail.com>
2024-07-21 20:06:08 +0530

    Add files via upload

---
+++ PROJECT - 1.py
@@ -0,0 +1,9 @@
+color=str(input('Enter the color'))
+if(color=="red"):
+    print('STOP')
+elif(color=="yellow"):
+    print('GET READY')
+elif(color=="green"):
+    print('GO')
+else:
+    print("Entered colour is invalid")
---
+++ PROJECT 2.py
@@ -0,0 +1,5 @@
+#Multiplication tables from 6 to 12 using for loop
+for i in range(6,13):
+    for j in range(1,11):
+       print(i,'*',j,'=',i*j)
+    print('______________________')
